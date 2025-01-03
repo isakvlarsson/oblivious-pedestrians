3c8c0aeb199bf4bfd5984bd7046ec106e66f5d78
Isak Larsson <isak.valter.larsson@gmail.com>
2025-01-03 18:25:17 +0100

    Add basic navigation and steering

--- agent.gd
+++ agent.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 @export var id: int
 @export var pref_vel: Vector2 
+@export var movement_speed = 1.0
 
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,3 +13,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func set_movement_target(movement_target: Vector2):
+	
+	navigation_agent.target_position = movement_target
+
+func _physics_process(delta):
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	pref_vel = current_agent_position.direction_to(next_path_position) * movement_speed
--- main.gd
+++ main.gd
@@ -10,13 +10,14 @@ func _ready() -> void:
 	for agent in agents:
 		var id = sim.AddAgent(agent)
 		agent.id = id
-		sim.SetAgentPreferedVelocity(id, agent.pref_vel)
+		agent.set_movement_target($Target.position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	for agent in agents:
 		var pos = sim.GetAgentPosition(agent.id)
 		agent.position = pos
+		sim.SetAgentPreferedVelocity(agent.id, agent.pref_vel)
 	
 func is_agent(node: Node2D) -> bool:
 	return node.is_in_group("agent")
